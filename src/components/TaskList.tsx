f3d235bea9a4b044c77ebea40dd2913bba01a520
EliasOPrado <eliaspradoprofessional@outlook.com>
2022-03-02 23:58:41 -0300

    updade: Added delete functionality.

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -39,10 +39,13 @@ export function TaskList() {
     Altere entre `true` ou `false` o campo `isComplete` 
     de uma task com dado ID
     */ 
+   console.log("Bateu aqui...", id)
+
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    setTasks(tasks => tasks.filter((item) => item.id !== id));
   }
 
   return (
